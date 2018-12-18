05f252748bec49319d4e8a5223415913f278ac48
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2018-12-18 23:07:39 +0200

    SSR fixes

--- server/index.ts
+++ server/index.ts
@@ -3,29 +3,41 @@ config();
 
 import Koa from "koa";
 import helmet from "koa-helmet";
+import Router from "koa-router";
+import nextInstance from "next";
 
 import logger from "./src/configurations/logger";
 
-const app = new Koa();
+const port = parseInt(process.env.PORT as string, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = nextInstance({ dev });
+const handle = app.getRequestHandler();
 
-app.use(helmet());
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
 
-app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    ctx.status = err.status || 500;
-    ctx.body = {
-      data: null,
-      error: err.message || "Internal server error",
-      status: ctx.status
-    };
-    ctx.app.emit("error", err, ctx);
-  }
+  server.use(helmet());
+  server.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      ctx.status = err.status || 500;
+      ctx.body = {
+        data: null,
+        error: err.message || "Internal server error",
+        status: ctx.status
+      };
+      ctx.app.emit("error", err, ctx);
+    }
+  });
+  server.on("error", err => logger.error(err));
+
+  router.get("*", async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(router.routes());
+  server.listen(port, () => logger.info(`Server running on port ${port}`));
 });
-
-app.on("error", err => logger.error(err));
-
-app.listen(process.env.PORT || 3000);
-
-logger.info(`Server running on port ${process.env.PORT || 3000}`);
